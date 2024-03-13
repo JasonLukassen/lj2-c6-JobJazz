6eceefb637c0e573f63b50233d8fa5c4b9414d2d
Diegooo <513642@vistacollege.nl>
2024-03-13 09:09:51 +0100

    Freelancer link to
    Freelancer link to

--- JobJazz/src/App.jsx
+++ JobJazz/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './pages/Home/Home'
 import Login from "./pages/AccountHandling/Login.jsx"
+import FreelancerProfile from "./pages/Freelancer/Freelancer.jsx"
 import FindWork from './pages/FindWork/FindWork.jsx'; // Assuming correct file path and component name
 import FindFreelancer from './pages/FindFreelancers/FindFreelancers.jsx'; // Assuming correct file path and component name
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="findwork" element={<FindWork />} />
         <Route path="findfreelancers" element={<FindFreelancer />} />
+        <Route path="freelancerprofile" element={<FreelancerProfile />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- JobJazz/src/pages/FindFreelancers/components/Card.jsx
+++ JobJazz/src/pages/FindFreelancers/components/Card.jsx
@@ -7,7 +7,9 @@ function Card() {
         <div className="w-56 h-40 bg-white shadow-xl rounded-3xl" >
             <div className=" inline-flex justify-evenly w-full mt-4">
                 <p className=" font-bold mt-1">Diego Loomans</p>
-                <img src="#" alt="Profile Picture" className=" w-10 h-10 bg-black rounded-full"/>
+                <Link to="/freelancerprofile">
+                    <img src="#" alt="Profile Picture" className="w-10 h-10 bg-black rounded-full" />
+                </Link>
             </div>
             <p className="ml-5 -mt-1">Software Developer</p>
             <Rating />
